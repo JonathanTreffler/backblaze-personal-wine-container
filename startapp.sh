ce58865d6beb1ecc9cd5c5a7e0eb90adfbd1e05b
Jonathan Treffler <mail@jonathan-treffler.de>
2022-01-16 02:18:20 +0100

    continued development

--- startapp.sh
+++ startapp.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -x
-whoami
-mkdir /config/wine/
-#mkdir -p /config/wine/dosdevices/
-cd /config/wine/
-wineboot --init
-curl -L "https://www.backblaze.com/win32/install_backblaze.exe" --output "install_backblaze.exe"
-ls -la
-wine64 "install_backblaze.exe" &
-#sleep 10
-#ln -s /drive_d/ /config/wine/dosdevices/d:
-sleep infinity
-#wine64 "$WINEPREFIX"/drive_c/Program\ Files/Backblaze/bzbui.exe -noqiet
+if [ -f "/config/wine/drive_c/Program Files (x86)/Backblaze/bzbui.exe" ]
+then
+    wine64 "/config/wine/drive_c/Program Files (x86)/Backblaze/bzbui.exe" -noqiet &
+    sleep infinity
+else
+    mkdir /config/wine/
+    cd /config/wine/
+    curl -L "https://www.backblaze.com/win32/install_backblaze.exe" --output "install_backblaze.exe"
+    ls -la
+    wine64 "install_backblaze.exe" &
+    #sleep 10
+    #ln -s /drive_d/ /config/wine/dosdevices/d:
+    sleep infinity
+fi
